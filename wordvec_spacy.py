248a8602cfad730faad42faa96684c1ad1699418
jeffsilverm <jeffsilverm@gmail.com>
2017-03-11 01:17:45 -0800

    Have it working as good as the examples on the web page, could be better

--- wordvec_spacy.py
+++ wordvec_spacy.py
@@ -1,13 +1,22 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
+from __future__ import print_function
+import sys
 import spacy
 import numpy
 
+print( "Creating an nlp object", file=sys.stderr )
 nlp = spacy.load('en')
+print ("Thinking....", file=sys.stderr )
 apples, and_, oranges = nlp(u'apples and oranges')
 print(apples.vector.shape)
 # (1,)
-apples.similarity(oranges)
+print(apples.similarity(oranges) )
+
+print ("Thinking....", file=sys.stderr )
+apples, and_, trains = nlp(u'apples and locomotives')
+print(apples.vector.shape)
+# (1,)
+print( apples.similarity(trains) )
 
